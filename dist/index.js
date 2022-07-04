c7e64e156adffb303184cc9acc5a9879d0b4cc7b
Viktor Videnov <victor.valentinov.videnov@gmail.com>
2022-07-04 11:20:37 +0300

    Fixes

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var Phone_1 = require("./src/classes/Phone");
 var models_1 = require("./src/models/models");
 function main() {
-    // const date = new Date()
     var phone = new Phone_1.Phone('3310', 'Nokia', 100, 'Viktor', {
         hoursIdle: 100,
         hours: 200,
@@ -12,14 +11,11 @@ function main() {
         size: 3,
         colors: 20,
     });
-    // const call: Call = new Call(new Date(), 222, 111)
+
     console.log(phone.phoneInfo());
     phone.phoneCallAction('Add')
     phone.phoneCallAction('Add')
     phone.phoneCallAction('Add')
     console.log(phone.callHistory);
-    // phone.phoneCallAction('Clear')
-    // console.log(phone.callHistory);
-    // console.log(phone.calculatePriceFromCalls());
 }
 main();
